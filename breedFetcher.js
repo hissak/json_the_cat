c15646bd058a147c8fa62cc10a85c11457d39638
vagrant <hassan.a.issak@gmail.com>
2023-07-06 20:34:36 +0000

    Refactors fetch function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
 const query = process.argv[2];
 const URL = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
-request(URL, function(error, response, body) {
-  // console.log(body) // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body);
-  // console.log('body type:', typeof body);
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  // console.log('data', data);
-  // console.log('data type:', typeof data);
-  console.log(data[0].description);// Print the HTML for the Google homepage.
+
+// request(URL, function(error, response, body) {
+//     if (error) {
+//       console.log(error);
+//     }
+//     const data = JSON.parse(body);
+//     console.log(data[0].description);
+//   });
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(URL, function(error, response, body) {
+    const data = JSON.parse(body);
+    callback(error, data[0].description)
 });
+}
+
+module.exports = {fetchBreedDescription}
---
+++ index.js
@@ -0,0 +1,10 @@
+const {fetchBreedDescription} = require('./breedFetcher');
+
+const breedName = process.argv[2]
+
+fetchBreedDescription(breedName, function(error, desc) {
+  if (error) {
+    console.log('Error details:', error)
+  }
+  console.log(desc)
+})
